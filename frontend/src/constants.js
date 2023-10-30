9565106feb5d8aa911fb31809821ca457590f424
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-10-31 02:01:51 +0300

    user auth endpoint-frontend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,3 +2,4 @@
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '/';
 export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/products';
+export const USERS_URL = '/api/users';
