29bb44b1a0f45e029200d8ed599d8fab5a12b9c1
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-10-30 21:59:16 +0300

    add protected middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 
@@ -10,6 +11,18 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+
+    // Set JWT as an HTTP-Only cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+      sameSite: 'strict', // Prevent CSRF attacks
+      maxAge: 24 * 60 * 60 * 1000, // 24 hour in milliseconds
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import products from './data/products.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -15,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
