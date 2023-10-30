104f806cfdcb0a11de1abc13f98a7a745086c16a
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-10-30 21:36:35 +0300

    user auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 
