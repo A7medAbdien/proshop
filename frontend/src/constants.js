9461f694776ba9d110c6abc21a30fa0ff87964a0
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-10-31 18:38:39 +0300

    order screen FRONTEND: consume endpoint and add UI BACKEND: implement order some of orders endpoints

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,3 +3,4 @@
 export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
+export const ORDERS_URL = '/api/orders';
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -44,9 +44,13 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       localStorage.setItem('cart', JSON.stringify(state));
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
   },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod, clearCartItems } = cartSlice.actions;
 
 export default cartSlice.reducer;
