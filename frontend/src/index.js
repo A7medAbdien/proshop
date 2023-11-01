d9576282e9926cf26a514070508bb40151354d91
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-11-01 16:59:54 +0300

    FRONTEND: add order screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
@@ -21,7 +22,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
-import PrivateRoute from './components/PrivateRoute';
+import OrderScreen from './screens/OrderScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,6 +37,7 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
       </Route>
     </Route>
   )
