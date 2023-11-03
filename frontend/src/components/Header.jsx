1f84a8d60773f268de95a63555442444caac4864
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-11-03 20:55:04 +0300

    ALL: admin users list & edit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -80,6 +80,9 @@ const Header = () => {
                                     <LinkContainer to='/admin/productlist'>
                                         <NavDropdown.Item>Products</NavDropdown.Item>
                                     </LinkContainer>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
                                 </NavDropdown>
                             )}
                         </Nav>
