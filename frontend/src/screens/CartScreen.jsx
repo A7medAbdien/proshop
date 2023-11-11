8c50204a484fb649d980d85a3170df2cd6e78764
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-11-11 14:36:29 +0300

    prefix displaying item.price -

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -51,7 +51,7 @@ const CartScreen = () => {
                   <Col md={3}>
                     <Link to={`/product/${item._id}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>${item.price.toFixed(2)}</Col>
                   <Col md={2}>
                     <Form.Control
                       as='select'
